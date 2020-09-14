13e335e8e3483c66ac6dc8899a51b0e9176d3972
parkermax93 <parker.williamson@gmail.com>
2020-09-14 15:51:21 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,39 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  state = {
+    username: '',
+    password: ''
+  }
 
-    this.state = {};
+  handleUsername = (e) => {
+    this.setState({username: e.target.value})
+  };
+
+  handlePassword = (e) => {
+    this.setState({password: e.target.value})
+  };
+
+  login = (event) => {
+    event.preventDefault();
+    if (this.state.username && this.state.password){
+      return this.props.handleLogin(this.state);
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.login}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleUsername} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handlePassword}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
+  state = {
+    message: ''
+  };
+  
+  handleInputChange = (e) => {
+    this.setState( {message: e.target.value})
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleInputChange}/>
+        <p>Remaining Characters: {this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
